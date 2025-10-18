b61f798bb3b18ed4f85e53ff9bab6cb043aaf91d
mohamed A nabawi <mo_nabawi@192.168.47.128>
2025-10-19 01:36:15 +0300

    Update

--- my-tech-app/app/app.py
+++ my-tech-app/app/app.py
@@ -2,22 +2,19 @@ import os
 import socket
 from flask import Flask, jsonify
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 app_version = os.environ.get('APP_VERSION', '1.0.0')
 hostname = socket.gethostname()
 
 @app.route('/')
 def get_info():
-  
     data = {
         'message': 'Welcome to My Tech App! 🚀',
         'version': app_version,
         'hostname': hostname
     }
-   
-    return jsonify(data) 
+    return jsonify(data)
 
-if _name_ == '_main_':
-   
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
